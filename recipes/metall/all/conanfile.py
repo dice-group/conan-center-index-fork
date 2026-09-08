f0a9db8909711fd460c06f86c1a03a1377eff2da
Alex <alexander@tentris.io>
2026-09-08 17:01:46 +0200

    metall: publish version 0.35, allow boost 1.91 from 0.35 on

--- recipes/metall/all/conanfile.py
+++ recipes/metall/all/conanfile.py
@@ -25,9 +25,12 @@ class MetallConan(ConanFile):
         basic_layout(self, src_folder="src")
 
     def requirements(self):
-        # boost 1.91 changed the signature of boost::interprocess named_proxy::construct_n,
-        # metall calls it with the signature of boost 1.90 and older
-        self.requires("boost/[>=1.81 <1.91]")
+        if Version(self.version) >= "0.35":
+            self.requires("boost/[>=1.81 <2]")
+        else:
+            # boost 1.91 changed the signature of boost::interprocess named_proxy::construct_n,
+            # metall before 0.35 calls it with the signature of boost 1.90 and older
+            self.requires("boost/[>=1.81 <1.91]")
 
     def package_id(self):
         self.info.clear()
